f93de1df632ae48a35b8a350f8612eb3496ae220
Hugh <hughhang30@gmail.com>
2024-03-13 23:39:13 -0400

    Finished

--- interaction.py
+++ interaction.py
@@ -2,61 +2,126 @@ import psycopg2
 import os
 from psycopg2 import sql
 
-def getDatabaseCredentials():
-    #Get environments to initialize connection
-    databaseName = os.environ.get('DB_NAME')
-    username = os.environ.get('USER')
-    password = os.environ.get('PASSWORD')
-    host = os.environ.get('HOST')
-    port = os.environ.get('PORT')
-    return databaseName, username, password, host, port
-
-#Set up the database connection
-databaseName, username, password, host, port = getDatabaseCredentials()
+#------CONNECTION SETUP------#
+#Set up database connection
 connection = psycopg2.connect(
-    dbname = databaseName,
-    user = username,
-    password = password,
-    host = host,
-    port = port
+    dbname = "Student", #os.environ.get('DB_NAME'),
+    user = "postgres", #os.environ.get('USER'),
+    password = "welcome", #os.environ.get('PASSWORD'),
+    host = "localhost", #os.environ.get('HOST'),
+    port = "5432" #os.environ.get('PORT')
 )
 
 #Create cursor to run SQL queries
 cursor = connection.cursor()
 
-#Create table
-def createTable():
-    cursor.execute(
-        """CREATE TABLE Students (
-	
-        student_id SERIAL PRIMARY KEY, 
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        enrollment_date DATE DEFAULT CURRENT_DATE
-	
+#-----TABLE SETUP-----#
+#Make the table if not already exist
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        student_id SERIAL PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        enrollment_date DATE
         );
+    """)
+connection.commit()
+
+#Check if already has data
+cursor.execute("SELECT COUNT(*) FROM students")
+
+#If data does not exist populate the table
+if (cursor.fetchone()[0] == 0):
+    cursor.execute(
+        """INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+        ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+        ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+        ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
         """)
+    connection.commit()
+
+#-----FUNCTIONS-----#
+
+#Get all students in table
+def getAllStudents():
     
+    #Get all students
+    cursor.execute("""SELECT * FROM STUDENTS""")
 
-#Populate table
-def populateTable():
+    #Print info of each student
+    for student in cursor.fetchall():
+        print("- - - - - - - - - -")
+        print("Student ID:     ", student[0])
+        print("First Name:     ", student[1])
+        print("Last Name:      ", student[2])
+        print("Email:          ", student[3])
+        print("Enrollment Date:", student[4])
 
-    #Check if already has data
-    cursor.execute("SELECT COUNT(*) FROM Students")
+    print("\n")
 
-    #If data does not exist populate the table
-    if (cursor.fetchone()[0] == 0):
-        cursor.execute(
-            """INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
-            ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
-            ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
-            ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
-            """)
+#Add new student to table
+def addStudent(fName, lName, email, enrollDate):
+
+    #Insert the new student info into the students table
+    cursor.execute("""INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES 
+                (%s, %s, %s, %s)""", (fName, lName, email, enrollDate))
+    connection.commit()
+
+    print("\nStudent", fName, lName, "added\n")
+        
+
+#Update a student's email
+def updateStudentEmail(email, id):
+
+    #Update the students email based on student's id
+    cursor.execute("""UPDATE students 
+                   SET email=%s 
+                   WHERE student_id=%s""", (email, id))
+    connection.commit()
+
+    print("\nStudent email updated \n")
+
+#Delete student from table
+def deleteStudent(id):
+    cursor.execute("""DELETE FROM students
+                   WHERE student_id=%s""", id)
+    connection.commit()
+
+    print("\nStudent deleted \n")
+
+while True:
+    print("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~")
+    operation = int(input("1. List all students \n2. Add a student \n3. Update student email \n4. Delete a student\n5. Quit\n\nPlease choose an operation: "))
+
+    match(operation):
+        #List all students
+        case 1: 
+            getAllStudents()
+
+    #     #Add a student
+        case 2:
+            first_name = input("New student's first name:      ")
+            last_name = input("New student's last name:       ")
+            email = input("New student's email:           ")
+            enrollment_date = input("New student's enrollment date: ")
+            addStudent(first_name, last_name, email, enrollment_date)
+
+    #     #Update student email
+        case 3:
+            id = input("Student's id:    ")
+            newEmail = input("Enter new email: ")
+            updateStudentEmail(newEmail, id)
+
+    #     #Delete a student
+        case 4:
+            id = input("ID of student to delete: ")
+            deleteStudent(id)
+
+    #     #Quit the app
+        case 5:
+            break
 
 
-#Main process starts here
-createTable()
-populateTable()
-
-print("test")
+cursor.close()
+connection.close()
